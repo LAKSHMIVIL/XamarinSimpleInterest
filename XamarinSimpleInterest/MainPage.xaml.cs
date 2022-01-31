dceb1dc3b62c38e08c16c4853c611895229698a8
LAKSHMIVIL <96649159+LAKSHMIVIL@users.noreply.github.com>
2022-02-01 01:21:36 +0530

    Xamarin Simple Interest Assignment

---
+++ XamarinSimpleInterest/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XamarinSimpleInterest
+{
+    public partial class MainPage : ContentPage
+    {
+        
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        public async void Simpleinterest(object sender, EventArgs e)
+        {
+            iname.Text = "" + int.Parse(pname.Text) * int.Parse(rname.Text) * int.Parse(tname.Text) / 100;
+
+        }
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
